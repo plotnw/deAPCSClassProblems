f1062a843c50b6286af347a809710d849c3ebe16
Burtorustum <aburur@d-e.org>
2016-04-25 15:21:02 -0400

    Fixed Stuff

--- Rob/APCSStuff/src/apcsstuff/APCSStuff.java
+++ Rob/APCSStuff/src/apcsstuff/APCSStuff.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package apcsproblems;
+package apcsstuff;
 
 /**
  *
  * @author aburur
  */
-public class APCSProblems {
+public class APCSStuff {
 
     /**
      * @param args the command line arguments
