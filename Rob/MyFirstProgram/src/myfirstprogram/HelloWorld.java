59b2599a668c5c62aa76b612adae8f75af1ce067
Burtorustum <aburur@d-e.org>
2016-09-12 07:58:40 -0400

    PogChamp Hello World PogChamp

--- Rob/MyFirstProgram/src/myfirstprogram/HelloWorld.java
+++ Rob/MyFirstProgram/src/myfirstprogram/HelloWorld.java
@@ -8,5 +8,8 @@ Introduction to input and output
 package myfirstprogram;
 public class HelloWorld 
 {
-    
+   public static void main(String [] args)
+   {
+       System.out.print("Hello, World!");
+   } 
 }
