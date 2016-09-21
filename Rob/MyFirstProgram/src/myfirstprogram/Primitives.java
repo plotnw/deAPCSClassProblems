5a26a6bc9fbcafd59e2c1e24a1af1b0767235b16
Burtorustum <aburur@d-e.org>
2016-09-21 11:54:37 -0400

    LUL

--- Rob/MyFirstProgram/src/myfirstprogram/Primitives.java
+++ Rob/MyFirstProgram/src/myfirstprogram/Primitives.java
@@ -1,6 +1,6 @@
 package myfirstprogram;
 /**
- * Playing with data types
+ * Playing with data types and stuff
  * @author Robert Aburustum
  * @version Sept. 2016
  */
@@ -13,9 +13,14 @@ public class Primitives
         double x = 4.3;
         System.out.println("n = "+ n);
         System.out.println("x = " + x);*/
-        double n = (double)100/3;
+        //double n = (double)100/3;
         //System.out.println(n);
-        System.out.printf("n = %8.2f", n);
-        
+        //System.out.printf("n = %8.2f", n);
+        long n = 1;
+        for (int c = 0; c < 100; c++)
+        {
+            n *= 2;
+            System.out.println(n);
+        }
     }
 }
